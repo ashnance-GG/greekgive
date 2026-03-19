b4c0d81b6f3e3ac0c92a5374faa107624d639502
ashnance-GG <ashnance@charter.net>
2026-03-19 17:29:09 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,4 +2,21 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
+  title: "greekgive",
+  description: "Fundraising, Made Simple.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        https://fonts.googleapis.com/css2?family=Zeyada&family=Arapey&display=swap
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
