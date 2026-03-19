89cfce80646ca56ae068afb99918d13e39d009dc
ashnance-GG <ashnance@charter.net>
2026-03-19 16:42:17 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Zeyada&family=Arapey&display=swap"
-        />
+        https://fonts.googleapis.com/css2?family=Zeyada&family=Arapey&display=swap
       </head>
       <body>{children}</body>
     </html>
